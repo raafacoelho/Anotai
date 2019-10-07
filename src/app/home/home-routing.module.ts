fb96435ffe0359baebcd985efae18ee8a9cd904c
Rafaella <ra_faellacoelho@hotmail.com>
2019-10-07 07:08:22 -0300

    home

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        loadChildren: './pages/home/home.module#HomePageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
