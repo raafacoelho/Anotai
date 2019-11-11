6a80e4983f2245de2625b2a2e9cd4d471cbf090b
raafacoelho <ra_faellacoelho@hotmail.com>
2019-11-11 16:35:20 -0300

    configurando a parte da conta do usuário

--- src/app/account/profile/profile.component.ts
+++ src/app/account/profile/profile.component.ts
@@ -1,19 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  public uploadPercent: Observable<number>;
+  public downloadUrl: Observable<string>;
 
-  constructor(private modalController: ModalController) { }
+  constructor(
+    private modalController: ModalController,
+    private camera: Camera,
+    private platform: Platform,
+    private file: File,
+    private afStorage: AngularFireStorage
+  ) {}
 
   ngOnInit() {}
+
+  async openGalery() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      correctOrientation: true
+    };
+
+    try {
+      const fileUri: string = await this.camera.getPicture(options);
+
+      let file: string;
+
+      if (this.platform.is('ios')) {
+        file = fileUri.split('/').pop();
+      } else {
+        file = fileUri.substring(fileUri.lastIndexOf('/'));
+      }
+
+      const path: string = fileUri.substring(0, fileUri.lastIndexOf('/'));
+
+      const buffer: ArrayBuffer = await this.file.readAsArrayBuffer(path, file);
+      const blob: Blob = new Blob([buffer], { type: 'image/jpeg' });
+
+      this.uploadPicture(blob);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  uploadPicture(blob: Blob) {
+    const ref = this.afStorage.ref('avatar/ionic.jpg');
+    const task = ref.put(blob);
+
+    this.uploadPercent = task.percentageChanges();
+
+    task.snapshotChanges().pipe(
+      finalize(() => this.downloadUrl = ref.getDownloadURL())
+    ).subscribe();
+  }
 
   async dismissModal() {
     this.modalController.dismiss();
   }
-
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,6 +9,10 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { Camera } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 import { environment } from 'src/environments/environment';
 
@@ -18,13 +22,16 @@ import { environment } from 'src/environments/environment';
     IonicModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
   ],
   exports: [BrowserModule, IonicModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    File
   ]
 })
 export class CoreModule {}
